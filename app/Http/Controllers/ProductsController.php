9f25f6b4759640f3b5e847869a345d52d7eef319
SOLOMON-PC\Solomon <solomon.iroegbu@gmail.com>
2020-06-16 17:45:42 +0100

    add products cred

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class ProductsController extends Controller
 {
@@ -26,6 +27,7 @@ class ProductsController extends Controller
     public function create()
     {
         //
+        return view('products.create');
     }
 
     /**
@@ -37,6 +39,31 @@ class ProductsController extends Controller
     public function store(Request $request)
     {
         //
+        $this->validate($request, [
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'image' => 'required|image'
+        ]);
+
+        $product = new Product;
+
+        $product_image = $request->image;
+
+        $product_image_new_name = time() . $product_image->getClientOriginalName();
+
+        $product_image->move('uploads/products', $product_image_new_name);
+
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->image = 'uploads/products/' . $product_image_new_name;
+
+        $product->save();
+
+        Session::flash('success', 'Product created.');
+
+        return redirect()->route('products.index');
     }
 
     /**
@@ -59,6 +86,7 @@ class ProductsController extends Controller
     public function edit($id)
     {
         //
+        return view('products.edit', ['product' => Product::find($id) ]);
     }
 
     /**
@@ -71,6 +99,37 @@ class ProductsController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $this->validate($request, [
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required'
+        ]);
+
+        $product = Product::find($id);
+
+        if($request->hasFile('image'))
+        {
+            $product_image = $request->image;
+
+            $product_image_new_name = time() . $product_image->getClientOriginalName();
+
+            $product_image->move('uploads/products', $product_image_new_name);
+
+            $product->image = 'uploads/products/' . $product_image_new_name;
+
+            $product->save();
+        }
+
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->price = $request->price;
+
+
+        $product->save();
+
+        Session::flash('success', 'Product updated.');
+
+        return redirect()->route('products.index');
     }
 
     /**
@@ -82,5 +141,17 @@ class ProductsController extends Controller
     public function destroy($id)
     {
         //
+        $product = Product::find($id);
+
+        if(file_exists($product->image))
+        {
+            unlink($product->image);
+        }
+
+        $product->delete();
+
+        Session::flash('success', 'Product deleted.');
+
+        return redirect()->back();
     }
 }
