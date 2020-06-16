6e28fdaa745f09a770d1b10a83f5bb7fd1554650
SOLOMON-PC\Solomon <solomon.iroegbu@gmail.com>
2020-06-16 23:17:46 +0100

    add composer require bumbummen99/shoppingcart shopping Web App functioning

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -8,6 +8,12 @@ use Illuminate\Support\Facades\Session;
 
 class ProductsController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
